0e58ca5a99ae9eed77fea0859c40165380926693
Mjadhav7218 <mjadhav@truxnow.com>
2025-02-05 08:58:06 +0530

    added loggers slf4j-api

--- src/main/java/com/example/Controller/StudentController.java
+++ src/main/java/com/example/Controller/StudentController.java
@@ -2,6 +2,8 @@ package com.example.Controller;
 
 import com.example.Model.Student;
 import com.example.Service.StudentService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,21 +20,26 @@ import java.util.List;
 @RequestMapping("/students")
 public class StudentController {
 
+    private Logger logger= LoggerFactory.getLogger(StudentController.class);
+
     @Autowired
     private StudentService studentService;
 
     @GetMapping
     public List<Student> getStudents(){
+        logger.info("Getting all the students");
         return studentService.getAllStudents();
     }
 
     @GetMapping("/{id}")
     public Student getStudentById(@PathVariable int id){
+        logger.info("Getting student data by Id");
         return studentService.getStudentById(id);
     }
 
     @PostMapping
     public String createStudent(@RequestBody Student student){
+        logger.info("Student is now creating");
          studentService.createStudent(student);
          return "Student created successfully";
     }
@@ -40,12 +47,14 @@ public class StudentController {
     @PutMapping("/{id}")
     public String updateStudent(@PathVariable int id, @RequestBody Student student){
         student.setId(id);
+        logger.info("Student is now updating");
         studentService.updateStudent(student);
         return "Student updated successfully";
     }
 
     @DeleteMapping("/{id}")
     public String deleteStudent(@PathVariable int id){
+        logger.info("Student is now deleting");
         studentService.deleteStudent(id);
         return "Student deleted successfully";
     }
--- src/main/java/com/example/Model/Student.java
+++ src/main/java/com/example/Model/Student.java
@@ -1,13 +1,14 @@
 package com.example.Model;
 
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Student {
     private int id;
     private String name;
